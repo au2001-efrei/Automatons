6d9ed7b7c7054b28bcf8e46074e941819b112035
Aurélien <au2001sfg@gmail.com>
2020-05-05 23:31:48 +0200

    Fix complementation

--- operations/complementation.py
+++ operations/complementation.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
 from .determinization import determinize
+from .completion import complete
 
 def complement(automaton):
-	# Copy the determinized automaton not to break the references to the previous one
-	automaton = determinize(automaton).copy()
+	# Copy the complete deterministic automaton not to break the references to the previous one
+	automaton = complete(determinize(automaton)).copy()
 
 	for state in automaton.states:
 		# Invert the terminal and non-terminal states
 		state.terminal = not state.terminal
+
+		if state.terminal:
+			automaton.terminal_states.add(state)
+		else:
+			automaton.terminal_states.remove(state)
 
 	return automaton
