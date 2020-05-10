f9c0be03410fdfe4db74ac91044346e75a8fc36d
Léa Buendé <46668755+leabuende@users.noreply.github.com>
2020-05-10 17:05:20 +0200

    comments on state class

--- automaton_types/state.py
+++ automaton_types/state.py
@@ -2,6 +2,8 @@
 
 class State(object):
 
+	#We initialize the object with the following structure: automaton, the name of the state, the incoming and outgoing
+	#transitions, such as info wether the state is terminal or initial or neither.
 	def __init__(self, automaton, state_id, initial=False, terminal=False):
 		super(State, self).__init__()
 
@@ -24,6 +26,8 @@ class State(object):
 
 		return State(automaton, self.state_id, self.initial, self.terminal)
 
+#Function to remove state, outgoing/incoming transitions, and to remove the state from
+#the initial/final state list if it was one
 	def remove(self):
 		for transition in self.transitions_from:
 			transition.remove()
@@ -37,6 +41,7 @@ class State(object):
 		if self.terminal:
 			self.automaton.terminal_states.remove(self)
 
+#Go through all outgoing transitions with a for loop to get the next state
 	def get_next_states(self, letter):
 		next_states = set()
 
@@ -46,6 +51,7 @@ class State(object):
 
 		return next_states
 
+#Go through all incoming transitions with a for loop to get the previous states
 	def get_previous_states(self, letter):
 		next_states = set()
 
